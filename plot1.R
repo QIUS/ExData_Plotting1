a8cc0111a42a8cd3b6d2a6b81ef12f4d79ae3428
Shiqiu Qiu <shiqiufxsq@gmail.com>
2014-06-08 10:38:42 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,12 @@
 ## an script for loading and creating png figure for part1 of project1
 
 # loading the data from working directory
-data1 <- read.table("./household_power_consumption.txt", header=TRUE, sep= ";", na.strings = "?", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+data1 <- read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = "?", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 
 # subsetting the data
 data1_sub <- data1[data1$Date == "1/2/2007" | data1$Date ==  "2/2/2007",]
 
 # making the plot to a png file
-png(filename = "./plot1.png", width = 480, height = 480, units = "px")
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
 hist(data1_sub$Global_active_power,xlab = "Global Active Power", main = "Global Active Power", col = "red")
 dev.off()
